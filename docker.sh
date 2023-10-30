38bb92977974cc26f304cec5af2424b95836615f
BeeBreeze <gdzhwlsh@gmail.com>
2023-10-30 15:50:18 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,6 @@
 docker run -it --name my_arm -v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro arm_ubuntu bash
 docker commit ID image_name
+docker build -t name:0.1 .
+docker run -ti hashID bash
+docker container rm --force $(docker container ps -aq)
+docker image prune
